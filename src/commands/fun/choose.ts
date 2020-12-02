42cf57b57efd2a1beecb5d881dc6041d6d777a21
unknown <paragdhruvincoc@gmail.com>
2020-12-02 12:43:55 +0530

    Fix the choice command behaving weirdly

--- src/commands/fun/choose.ts
+++ src/commands/fun/choose.ts
@@ -1,6 +1,5 @@
 import 'discord-akairo'
 import { Command } from 'discord-akairo'
-import { MessageEmbed } from 'discord.js'
 import { Message } from 'discord.js'
 import fn from '../../utils/functions'
 
@@ -25,26 +24,24 @@ export default class Draw extends Command{
         })
     }
 
-    public exec(message: Message, { args }: { args: string[]}): Promise<Message> {
-        args = fn.cleanArray(args)
+    public exec(message: Message, { args }: { args: string}): Promise<Message> {
 
         /**
-         * Embeds
+         * This check has to go first with it's respective text
          */
-        const give_something_to_choose_from = new MessageEmbed()
-        .setDescription(`Atleast give me something to choose from`)
-
-        const thats_what_i_choose_if_u_dont_give_me_choices = new MessageEmbed()
-        .setDescription(`Well i chose ${args[0]} as you did not give me any other choice`)
-
-        const its_my_choice = new MessageEmbed()
-        .setDescription(`I Chose \`${fn.choice(args)}\``)
+        const give_something_to_choose_from = `Next time when you run this command, give me some options to choose from`
+        if (!fn.cleanArray(args.split(" "))) return message.channel.send(give_something_to_choose_from)
+        
+        /**
+         * Texts look better than embeds
+         */
+        const thats_what_i_choose_if_u_dont_give_me_choices = `Well i chose \`${fn.cleanArray(args.split(" "))[0]}\` as you did not give me any other choice`
+        const its_my_choice = `\`${fn.choice(fn.cleanArray(args.split(" ")))}\``
 
         /**
-         * Checks
+         * Checks and result
          */
-        if (!args) return message.channel.send(give_something_to_choose_from)
-        if(args.length === 1) return message.channel.send(thats_what_i_choose_if_u_dont_give_me_choices)
+        if(fn.cleanArray(args.split(" ")).length === 1) return message.channel.send(thats_what_i_choose_if_u_dont_give_me_choices)
         else return message.channel.send(its_my_choice)
 
     }
