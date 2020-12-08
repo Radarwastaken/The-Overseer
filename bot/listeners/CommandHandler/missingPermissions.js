c8c5dec46407455c10c7eb803ef64b04504ce198
unknown <paragdhruvincoc@gmail.com>
2020-12-08 16:30:23 +0530

    Added Missing Permissions event

---
+++ bot/listeners/CommandHandler/missingPermissions.js
@@ -0,0 +1,35 @@
+const { Listener, Command } = require('discord-akairo')
+const { MessageEmbed } = require('discord.js')
+const permissions = require('../../assets/permssions.json')
+const emojis = require('../../utils/emojis.json')
+
+export default class Cooldown extends Listener {
+    constructor() {
+        super("missingPermissions", {
+            emitter: "cmdHandler",
+            event: "missingPermissions",
+            category: "command"
+        })
+    }
+
+    exec(message, command, type, missing) {
+
+        let perm = missing.map((p) => permissions[p])
+
+        if (type === 'client') {
+        const embed = new MessageEmbed()
+        .setAuthor(`Missing Permssions`)
+        .setDescription(`${emojis.discord.Preferences}${message.author} I need the \`${(perm.map((p) => p)).join(`, `)}\` Permission(s) to run the ${command} Command`)
+        return message.channel.send(embed)
+        }
+        else if (type === 'user') {
+            const embedtoo = new MessageEmbed()
+            .setAuthor(`Missing Permissions`)
+            .setDescription(`${emojis.discord.CrossMark}${message.author} You need the \`${(perm.map((p) => p)).join(`, `)}\` Permission(s) to use the ${command} Command`)
+            return message.channel.send(embedtoo)
+        }
+        else {
+            return message.channel.send(`That is sus and is not supposed to happen...(maybe it is an error)`)
+        }
+    }
+}
