f60b2e50e4758e89e3b66654ba2053dc65b191c6
unknown <paragdhruvincoc@gmail.com>
2020-12-08 10:50:18 +0530

    started working on rewrite...

---
+++ bot/index.js
@@ -0,0 +1,5 @@
+const VilcusClient = require('./structures/Client')
+
+const VilcusClientem = new VilcusClient()
+
+VilcusClientem.start()
---
+++ bot/structures/Client.js
@@ -0,0 +1,66 @@
+const ak = require('discord-akairo')
+const config = require('./../config.json')
+
+class VilicusClient extends ak.AkairoClient {
+    constructor(Config) {
+        super({
+            ownerID: config.bot.ownerids,
+        })
+        this.ListenerHandler = new ak.ListenerHandler(this, {
+            directory: './listeners'
+        })
+        this.CommandHandler = new ak.CommandHandler(this, {
+            directory: './commands',
+            prefix: '+',
+            allowMention: true,
+            blockBots: true,
+            blockClient: true,
+            commandUtil: true,
+            argumentDefaults: {
+            prompt: {
+                modifyStart: (_, str) => `${str}\n\nType \`stop\` to stop the command.`,
+                retry: `Try again...`,
+                timeout: `Alright then Ignore me even i don't have enough free time to keep waiting for you.`,
+                ended: `You failed too many times, no more trying again now.`,
+                cancel: `Okay stopped the command.`,
+                cancelWord: 'stop',
+                retries: 3,
+                time: 3e4
+                }
+            },
+            automateCategories: true
+        })
+        this.Config = Config
+
+        this.token = config.bot.token
+
+        this.bsapi = config.api.bs
+
+        this.cocapi = config.api.coc
+
+        this.crapi = config.api.cr
+
+        this.ghapi = config.api.gh
+
+        this.fn = require('../utils/functions')
+
+    }
+
+    async _init() {
+        this.CommandHandler.useListenerHandler(this.ListenerHandler)
+        this.ListenerHandler.setEmitters({
+            CommandHandler: this.CommandHandler,
+            ListenerHandler: this.ListenerHandler,
+            process
+        })
+        this.CommandHandler.loadAll()
+        this.ListenerHandler.loadAll()
+    }
+
+    async start() {
+        await this._init()
+        return this.login(this.token)
+    }
+}
+
+module.exports = VilicusClient
