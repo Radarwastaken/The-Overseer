96a6a2e94477b6e257d0ce6fdc79eb4ff135da97
unknown <paragdhruvincoc@gmail.com>
2020-12-12 11:27:02 +0530

    Removed unecessary packages and added auto-server count posting on top.gg

--- bot/index.js
+++ bot/index.js
@@ -2,4 +2,11 @@ const VilcusClient = require('./structures/Client')
 
 const client = new VilcusClient()
 
+//Top.GG Server count stuff Ignore...
+const AutoPoster = require('topgg-autoposter')
+const config = require('./config.json')
+
+const poster = new AutoPoster(config.botlist.top, client)
+//Over
+
 client.start()
